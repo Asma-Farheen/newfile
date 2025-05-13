e7839ecaa88d5c6abfecad84547f16d0587c388c
Asma-Farheen <asmafarheen2025@gmail.com>
2025-05-13 13:57:04 +0530

    updated sql file

--- schema.sql
+++ schema.sql
@@ -1,6 +1,12 @@
-CREATE TABLE UMeStore(id INTEGER PRIMARY KEY, Name TEXT,Price INTEGER , StoneColour TEXT , quantity INTEGER);
+CREATE TABLE UMeStore (
+  id INTEGER PRIMARY KEY,
+  Name TEXT,
+  Price INTEGER,
+  StoneColour TEXT,
+  quantity INTEGER
+);
 
-INSERT INTO UMeStore VALUES(1 , "Chain", 200 ,"Red", 6);
-INSERT INTO UMeStore VALUES(2,"Ring",100,"White",3);
-INSERT INTO UMeStore VALUES(3, "Wallet",  100," Black",2 );
-INSERT INTO UMeStore VALUES(4, "Braclet",50, "Brown",5);
+INSERT INTO UMeStore VALUES (1, "Chain", 200, "Red", 6);
+INSERT INTO UMeStore VALUES (2, "Ring", 100, "White", 3);
+INSERT INTO UMeStore VALUES (3, "Wallet", 100, "Black", 2);
+INSERT INTO UMeStore VALUES (4, "Braclet", 50, "Brown", 5);
